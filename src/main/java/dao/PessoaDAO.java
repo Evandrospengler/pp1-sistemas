081d84ef12778d68ff54def70ee9a1f5cef3059d
Evandro <spenglerevandro@gmail.com>
2018-11-05 20:54:37 -0200

    novos arquivos

--- src/main/java/dao/PessoaDAO.java
+++ src/main/java/dao/PessoaDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import config.HibernateUtil;
@@ -10,10 +5,7 @@ import java.util.List;
 import model.Pessoa;
 import org.hibernate.Session;
 
-/**
- *
- * @author Anderson
- */
+
 public class PessoaDAO {
 
     private Session session;
